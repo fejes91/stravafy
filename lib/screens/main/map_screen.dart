233cc3896abf4a1bf74992c11d4b777107b38284
Adam Fejes <adam.fejes@supercharge.io>
2023-01-14 19:59:29 +0100

    Setup an empty map screen

---
+++ lib/screens/main/map_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stravafy'),
+      ),
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(47.509364, 19.128928),
+          zoom: 9,
+        ),
+        nonRotatedChildren: [],
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'hu.adamfejes.stravafy',
+          ),
+        ],
+      ),
+    );
+  }
+}
