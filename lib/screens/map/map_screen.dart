8fa8b147623093cf97fb52d9e8a5f0c103de16de
Adam Fejes <adam.fejes@supercharge.io>
2023-01-15 11:26:43 +0100

    Adjust map to fit on polyline

--- lib/screens/map/map_screen.dart
+++ lib/screens/map/map_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:get_it/get_it.dart';
@@ -13,6 +15,8 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  StreamSubscription? _polylineSubscription;
+  final MapController _mapController = MapController();
   late MapScreenVM _viewModel;
 
   @override
@@ -22,8 +26,15 @@ class _MapScreenState extends State<MapScreen> {
     _viewModel = GetIt.I<MapScreenVM>();
   }
 
+  void onMapReady() {
+    _polylineSubscription = _viewModel.polylines.listen((polylinePoints) {
+      _adjustMap(polylinePoints);
+    });
+  }
+
   @override
   void dispose() {
+    _polylineSubscription?.cancel();
     _viewModel.dispose();
     super.dispose();
   }
@@ -35,7 +46,9 @@ class _MapScreenState extends State<MapScreen> {
         title: const Text('Stravafy'),
       ),
       body: FlutterMap(
+        mapController: _mapController,
         options: MapOptions(
+          onMapReady: onMapReady,
           center: LatLng(47.509364, 19.128928),
           zoom: 9,
         ),
@@ -48,21 +61,32 @@ class _MapScreenState extends State<MapScreen> {
           StreamBuilder<List<LatLng>>(
             initialData: const <LatLng>[],
             stream: _viewModel.polylines,
-            builder: (context, polyline) => PolylineLayer(
-              polylineCulling: true,
-              polylines: [
-                Polyline(
-                  points: polyline.data!,
-                  color: Colors.red,
-                  strokeWidth: 2.5,
-                  borderStrokeWidth: 2.0,
-                  borderColor: Colors.white,
-                ),
-              ],
-            ),
+            builder: (context, polyline) {
+              return _getPolylineLayer(polyline.data!);
+            },
           )
         ],
       ),
     );
+  }
+
+  PolylineLayer _getPolylineLayer(List<LatLng> polyline) {
+    return PolylineLayer(
+      polylineCulling: true,
+      polylines: [
+        Polyline(
+          points: polyline,
+          color: Colors.red,
+          strokeWidth: 2.5,
+          borderStrokeWidth: 2.0,
+          borderColor: Colors.white,
+        ),
+      ],
+    );
+  }
+
+  void _adjustMap(List<LatLng> points) {
+    _mapController.fitBounds(LatLngBounds.fromPoints(points),
+        options: const FitBoundsOptions(padding: EdgeInsets.all(32)));
   }
 }
